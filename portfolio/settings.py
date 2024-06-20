ab861873e97a0650f2060c15137a823d14921574
BuildTools <unconfigured@null.spigotmc.org>
2024-06-20 18:07:08 -0400

    Added back DEBUG env variable

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -28,7 +28,7 @@ load_dotenv()
 SECRET_KEY = os.environ.get('SECRET_KEY', get_random_secret_key())
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True #os.environ.get('DEBUG', 'False') == 'True'
+DEBUG = os.environ.get('DEBUG', 'False') == 'True'
 ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', 'localhost').split(',')
 
 
