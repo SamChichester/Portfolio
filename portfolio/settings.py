66579d581adcf74ccb6c43801a2cb5f023534bea
BuildTools <unconfigured@null.spigotmc.org>
2024-06-20 17:41:53 -0400

    Added SECURE_SSL_REDIRECT and adjusted static root

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -64,7 +64,7 @@ SECURE_BROWSER_XSS_FILTER = True
 SECURE_CONTENT_TYPE_NOSNIFF = True
 
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
-SECURE_SSL_REDIRECT = False
+SECURE_SSL_REDIRECT = True
 
 SECURE_HSTS_SECONDS = 86400
 SECURE_HSTS_PRELOAD = True
@@ -140,7 +140,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = os.environ.get('STATIC_URL', '/static/')
+STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Default primary key field type
